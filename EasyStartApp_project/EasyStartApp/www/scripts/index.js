36050b47bc596cc9c750d12ab7fe0570818fb452
sharpmsil@gmail.com <sharpmsil@gmail.com>
2019-04-23 20:59:37 +0300

    Setting project

--- EasyStartApp_project/EasyStartApp/www/scripts/index.js
+++ EasyStartApp_project/EasyStartApp/www/scripts/index.js
@@ -1,29 +1,14 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkID=397704
-// To debug code on page load in cordova-simulate or on Android devices/emulators: launch your app, set breakpoints,
-// and then run "window.location.reload()" in the JavaScript Console.
-(function () {
-    "use strict";
-
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
-
-    function onDeviceReady() {
-        // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-    };
-
-    function onPause() {
-        // TODO: This application has been suspended. Save application state here.
-    };
-
-    function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
-    };
-})();
-
-$(document).ready(function () {
-    setTimeout(() => $.mobile.changePage("#catalog", { transition: "none" }), 100);
+﻿$(document).ready(function () {
+    setTimeout(function () { $.mobile.changePage("#catalog", { transition: "none" }) }, 500);
+    document.addEventListener('deviceready', onDeviceReady, false);
 });
+
+function onDeviceReady() {
+    document.addEventListener("backbutton", onBackKeyDown, false);
+};
+
+function onBackKeyDown() {
+    if ($.mobile.activePage.is("#products")) {
+        navigator.app.backHistory()
+    }
+}
